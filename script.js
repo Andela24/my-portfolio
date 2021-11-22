56b53f7c5520a1b4512d465a579b44a894d10eb0
Andela Stevanovic <andjela.s166@gmail.com>
2021-11-22 11:45:38 -0500

    add script.js, connected to index.html and style.css

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 const h2 = document.createElement("h2");
 h2.textContent = "WELCOME TO MY PERSONAL PAGE";
+
+const image = document.getElementById('image');
+image.addEventListener('click', function() {
+    alert('Don not touch my coffee!');
+});
+let h4 = document.getElementsByTagName('h4');
+h4.addEventListener('mouseover', function(event) {
+    event.target.style.color = "orange";
+
+    setTimeout(function() {
+        event.target.style.color = "";
+    }, 500);
+    }, false);
